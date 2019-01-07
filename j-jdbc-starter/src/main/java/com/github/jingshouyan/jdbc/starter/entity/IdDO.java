983a7c9d27091ee83e4dfaa606cce120ad17ac80
jingshouyan <jingshouyan@gmail.com>
2019-01-07 17:28:09 +0800

    error log

--- j-jdbc-starter/src/main/java/com/github/jingshouyan/jdbc/starter/entity/IdDO.java
+++ j-jdbc-starter/src/main/java/com/github/jingshouyan/jdbc/starter/entity/IdDO.java
@@ -2,6 +2,7 @@ package com.github.jingshouyan.jdbc.starter.entity;
 
 import com.github.jingshouyan.jdbc.comm.annotaion.Column;
 import com.github.jingshouyan.jdbc.comm.annotaion.Key;
+import com.github.jingshouyan.jdbc.comm.annotaion.Table;
 import com.github.jingshouyan.jdbc.comm.entity.BaseDO;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,6 +13,7 @@ import lombok.ToString;
  * 11/29/18 4:21 PM
  */
 @Getter@Setter@ToString
+@Table("ID_SEED")
 public class IdDO extends BaseDO {
     @Key
     @Column(length = 50,comment = "种子类型")
