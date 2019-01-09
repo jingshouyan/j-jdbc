19775e2f79bbf9482ec228eafa272209ddb4a6fc
靖守彦 <290173092@qq.com>
2019-01-09 16:43:26 +0800

    test

--- j-jdbc-test/src/main/java/com/jingshouyan/config/AppConfig.java
+++ j-jdbc-test/src/main/java/com/jingshouyan/config/AppConfig.java
@@ -16,9 +16,9 @@ import javax.sql.DataSource;
 public class AppConfig {
 
 
-    public static final String url="jdbc:mysql://127.0.0.1:3306/IM_USER?useUnicode=true&characterEncoding=utf8&useSSL=false";
-    public static final String username="jing";
-    public static final String password="Jing1234!@#$";
+    public static final String url="jdbc:mysql://127.0.0.1:3306/test?useUnicode=true&characterEncoding=utf8&useSSL=false";
+    public static final String username="root";
+    public static final String password="abcd1234";
     public static final String driver="com.mysql.jdbc.Driver";
 
     @Bean
