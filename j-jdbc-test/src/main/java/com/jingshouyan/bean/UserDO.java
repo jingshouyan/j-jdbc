f683ceb9e11f3662c7f1187f07028dca2a1cf1d9
jingshouyan <jingshouyan@gmail.com>
2019-05-06 19:36:12 +0800

    code(ajcg): 代码规范扫描

--- j-jdbc-test/src/main/java/com/jingshouyan/bean/UserDO.java
+++ j-jdbc-test/src/main/java/com/jingshouyan/bean/UserDO.java
@@ -9,34 +9,54 @@ import lombok.ToString;
 
 import java.util.List;
 
+
+@SuppressWarnings("AlibabaCommentsMustBeJavadocFormat")
+@Getter@Setter@ToString
+/**
+ * 表注解,指定表明,不加默认为类名
+ */
+
+@Table(value = "DEMO_USER",comment = "用户表")
+/**
+ * @Index({"age","nickname"})
+ * @Index({"id","name"})
+ */
 /**
  * @author jingshouyan
  * 11/29/18 5:20 PM
  */
-@Getter@Setter@ToString
-//表注解,指定表明,不加默认为类名
-@Table(value = "DEMO_USER",comment = "用户表")
-//@Index({"age","nickname"})
-//@Index({"id","name"})
 @Indices({@Index({"id","name"}),@Index({"age","nickname"})})
 public class UserDO extends BaseDO {
-    //主键注解,主键只能是Long/String,且只能有一个,若没有主键 find/findByIds 方法不能使用
-    //默认当未设值(为 null)时,只用id生成器自动设值
+
+    /**
+     * 主键注解,主键只能是Long/String,且只能有一个,若没有主键 find/findByIds 方法不能使用
+     * 默认当未设值(为 null)时,只用id生成器自动设值
+     * 列注解,设置列长度
+     */
     @Key(generatorIfNotSet = true)
-    //列注解,设置列长度
     @Column(length = 50,comment = "主键")
     private String id;
-    // 列注解,选择加密方式为属性id的值为key
-    // 使用加密后,无法作为查询条件,因为数据库中存放的是密文
+    /**
+     * 列注解,选择加密方式为属性id的值为key
+     * 使用加密后,无法作为查询条件,因为数据库中存放的是密文
+     */
     @Column(encryptType = EncryptType.FLIED,encryptKey = "id")
     private String name;
-    //列类型使用包装类型,因为在更新操作时 null 值不更新
+    /**
+     * 列类型使用包装类型,因为在更新操作时 null 值不更新
+     */
+
     private Integer age;
-    //列注解,选择加密方式为固定值abcd
-    //数据以json格式入库,本例为 json 字符串加密后存储,未来可能移除json同时又是加密字段的模式
+    /**
+     *列注解,选择加密方式为固定值abcd
+     * 数据以json格式入库,本例为 json 字符串加密后存储,
+     * 未来可能移除json同时又是加密字段的模式
+     */
     @Column(encryptType = EncryptType.FIXED,encryptKey = "abcd",json = true,length = 1000)
     private List<String> tags;
-    //设置数据库字段名,默认为属性名
+    /**
+     *设置数据库字段名,默认为属性名
+     */
     @Column(value = "NICK_NAME_TT4")
     private String nickname;
     @Column(value = "ENCRYPT_TEST",encryptType = EncryptType.FIXED,encryptKey = "abcdeeee1123")
