6d4af121a63d36f72ad031143e568768e18197c6
靖守彦 <290173092@qq.com>
2019-05-21 18:49:44 +0800

    feat(sql): rm sql generator 4 gbase,kingbase,oracle

--- j-jdbc-core/src/main/java/com/github/jingshouyan/jdbc/core/sql/generator/factory/SqlGeneratorFactoryUtil.java
+++ j-jdbc-core/src/main/java/com/github/jingshouyan/jdbc/core/sql/generator/factory/SqlGeneratorFactoryUtil.java
@@ -1,9 +1,6 @@
 package com.github.jingshouyan.jdbc.core.sql.generator.factory;
 
-import com.github.jingshouyan.jdbc.core.sql.generator.SqlGenerator4Gbase;
-import com.github.jingshouyan.jdbc.core.sql.generator.SqlGenerator4Kingbase;
 import com.github.jingshouyan.jdbc.core.sql.generator.SqlGenerator4Mysql;
-import com.github.jingshouyan.jdbc.core.sql.generator.SqlGenerator4Oracle;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,9 +11,6 @@ import lombok.Setter;
 public class SqlGeneratorFactoryUtil {
 
     public static final SqlGeneratorFactory MYSQL = SqlGenerator4Mysql::new;
-    public static final SqlGeneratorFactory ORACLE = SqlGenerator4Oracle::new;
-    public static final SqlGeneratorFactory G_BASE = SqlGenerator4Gbase::new;
-    public static final SqlGeneratorFactory KING_BASE = SqlGenerator4Kingbase::new;
 
     @Getter@Setter
     private static SqlGeneratorFactory sqlGeneratorFactory = MYSQL;
