868bb654c984c8cf8bb6a008e83e242da71d68b0
jingshouyan <jingshouyan@gmail.com>
2019-05-30 11:02:39 +0800

    perf(sql): sql generator factory use memory cache

--- j-jdbc-core/src/main/java/com/github/jingshouyan/jdbc/core/sql/generator/factory/SqlGeneratorFactoryUtil.java
+++ j-jdbc-core/src/main/java/com/github/jingshouyan/jdbc/core/sql/generator/factory/SqlGeneratorFactoryUtil.java
@@ -1,6 +1,7 @@
 package com.github.jingshouyan.jdbc.core.sql.generator.factory;
 
 import com.github.jingshouyan.jdbc.core.sql.generator.SqlGenerator4Mysql;
+import com.github.jingshouyan.jdbc.core.sql.generator.factory.mysql.MysqlSqlGeneratorFactory;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,7 +11,7 @@ import lombok.Setter;
  */
 public class SqlGeneratorFactoryUtil {
 
-    public static final SqlGeneratorFactory MYSQL = SqlGenerator4Mysql::new;
+    public static final SqlGeneratorFactory MYSQL = new MysqlSqlGeneratorFactory();
 
     @Getter@Setter
     private static SqlGeneratorFactory sqlGeneratorFactory = MYSQL;
---
+++ j-jdbc-core/src/main/java/com/github/jingshouyan/jdbc/core/sql/generator/factory/mysql/MysqlSqlGeneratorFactory.java
@@ -0,0 +1,22 @@
+package com.github.jingshouyan.jdbc.core.sql.generator.factory.mysql;
+
+import com.github.jingshouyan.jdbc.core.sql.generator.SqlGenerator;
+import com.github.jingshouyan.jdbc.core.sql.generator.SqlGenerator4Mysql;
+import com.github.jingshouyan.jdbc.core.sql.generator.factory.SqlGeneratorFactory;
+import com.google.common.collect.Maps;
+
+import java.util.Map;
+
+/**
+ * @author jingshouyan
+ * #date 2019/5/29 18:32
+ */
+
+public class MysqlSqlGeneratorFactory implements SqlGeneratorFactory {
+    private static final Map<Class<?>,SqlGenerator<?>> GENERATOR_CACHE = Maps.newConcurrentMap();
+
+    @Override
+    public <T> SqlGenerator<T> sqlGenerator(Class<T> clazz) {
+        return ( SqlGenerator<T>) GENERATOR_CACHE.computeIfAbsent(clazz, SqlGenerator4Mysql::new);
+    }
+}
