96c76555b2f458b6f686ea3a63c355e85545e7b8
jingshouyan <jingshouyan@gmail.com>
2020-04-09 16:51:56 +0800

    docs(common): 添加注释

--- j-jdbc-common/src/main/java/com/github/jingshouyan/jdbc/comm/annotation/Foreign.java
+++ j-jdbc-common/src/main/java/com/github/jingshouyan/jdbc/comm/annotation/Foreign.java
@@ -2,6 +2,10 @@ package com.github.jingshouyan.jdbc.comm.annotation;
 
 import java.lang.annotation.*;
 
+/**
+ * @author jingshouyan
+ * #date 2019/1/30 11:53
+ */
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
