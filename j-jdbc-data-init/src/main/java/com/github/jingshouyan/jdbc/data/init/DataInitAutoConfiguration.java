ce5f201533a7fef069098f27ac3b0327266bad76
jingshouyan <jingshouyan@gmail.com>
2020-04-14 19:29:55 +0800

    feat(data-init): 执行初始化接口

--- j-jdbc-data-init/src/main/java/com/github/jingshouyan/jdbc/data/init/DataInitAutoConfiguration.java
+++ j-jdbc-data-init/src/main/java/com/github/jingshouyan/jdbc/data/init/DataInitAutoConfiguration.java
@@ -1,20 +1,25 @@
 package com.github.jingshouyan.jdbc.data.init;
 
 import com.github.jingshouyan.jdbc.comm.util.VersionUtil;
+import com.github.jingshouyan.jdbc.data.init.action.InitData;
 import com.github.jingshouyan.jdbc.data.init.dao.DataInitVersionDao;
 import com.github.jingshouyan.jdbc.data.init.entity.DataInitVersion;
+import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 
 import javax.annotation.Resource;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 /**
@@ -34,8 +39,14 @@ public class DataInitAutoConfiguration implements ApplicationRunner {
     @Autowired
     private DataInitVersionDao versionDao;
 
+    @Autowired
+    private ApplicationContext ctx;
+
     @Override
-    public void run(ApplicationArguments args) throws Exception {
+    public void run(ApplicationArguments args) {
+        Map<String,InitData> map = ctx.getBeansOfType(InitData.class);
+        List<InitData> actions = Lists.newArrayList(map.values());
+        Collections.sort(actions);
         Optional<DataInitVersion> optVersion = versionDao.latestVersion();
         List<String> versions = properties.getVersions();
         if (optVersion.isPresent()) {
@@ -46,7 +57,7 @@ public class DataInitAutoConfiguration implements ApplicationRunner {
                         DataInitVersion dataInitVersion = new DataInitVersion();
                         dataInitVersion.setVersion(v);
                         versionDao.insert(dataInitVersion);
-
+                        actions.forEach(act -> act.action(dataInitVersion));
 
                     });
         }
