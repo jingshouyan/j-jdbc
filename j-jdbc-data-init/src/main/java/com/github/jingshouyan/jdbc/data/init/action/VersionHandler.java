f3eb12f925bb921daccc7d7a2ce1e8245da2033b
Jing Shouyan <290173092@qq.com>
2020-04-15 15:04:51 +0800

    feat(data-init): 基于接口执行初始化

---
+++ j-jdbc-data-init/src/main/java/com/github/jingshouyan/jdbc/data/init/action/VersionHandler.java
@@ -0,0 +1,32 @@
+package com.github.jingshouyan.jdbc.data.init.action;
+
+import com.github.jingshouyan.jdbc.comm.util.VersionUtil;
+
+/**
+ * @author jingshouyan
+ * #date 2020/4/14 18:56
+ */
+public interface VersionHandler extends Comparable<VersionHandler> {
+
+    /**
+     * 对应的版本
+     * @return 版本
+     */
+    String version();
+    /**
+     * 执行该版本的
+     */
+    void action();
+
+    /**
+     * 比较大小
+     * @param o other
+     * @return 大小
+     */
+    @Override
+    default int compareTo(VersionHandler o){
+        return VersionUtil.compareVersion(this.version(),o.version());
+    }
+
+
+}
--- j-jdbc-data-init/src/main/java/com/github/jingshouyan/jdbc/data/init/entity/DataInitVersion.java
+++ j-jdbc-data-init/src/main/java/com/github/jingshouyan/jdbc/data/init/entity/DataInitVersion.java
@@ -3,6 +3,7 @@ package com.github.jingshouyan.jdbc.data.init.entity;
 import com.github.jingshouyan.jdbc.comm.annotation.Column;
 import com.github.jingshouyan.jdbc.comm.annotation.Key;
 import com.github.jingshouyan.jdbc.comm.annotation.Table;
+import com.github.jingshouyan.jdbc.comm.bean.DataType;
 import com.github.jingshouyan.jdbc.comm.entity.BaseDO;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -20,5 +21,11 @@ public class DataInitVersion extends BaseDO {
 
     @Column(length = 50, comment = "版本")
     private String version;
+    @Column(length = 500, comment = "执行的类")
+    private String clazz;
+    @Column(length = 50, comment = "执行结果")
+    private Boolean success;
+    @Column(dataType = DataType.TEXT,comment = "错误信息")
+    private String message;
 
 }
